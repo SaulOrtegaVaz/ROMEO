1a015d271c59c6ff3990f10d9bfac1fe32758f84
SaulOrtegaVaz <darkstormof_fire@hotmail.com>
2018-04-11 19:57:40 +0200

    Programa Control en prueba
    Control como AP dando web para recibir ssid y pass de red externa a la que debe conectarse y guardado en EEPROM.
    Estructura en ControlAP y ControlEstacion

--- Programa_Prueba/Programa_Prueba.ino
+++ Programa_Prueba/Programa_Prueba.ino
@@ -1,144 +1,253 @@
 /* 
- *  DATOS IMPORTANTES *
- ******************************************************************************************************************
- *** USO DE STRING mediate entrada de teclado PARA CONECTARSE A LAS REDES ***
- Conexion mediante variables tipo STRING => WiFi.begin(ssid.substring(0, ssid.length()-2).c_str(), pass.substring(0, pass.length()-2).c_str());
- Hay que restar 2 a la longitud de la cadena para restar el caracter de fin de cadena \n que contiene cada STRING
- También hay que usar la funcion c_str() para convertir la cadena de tipo STRING a tipo char ???[X] que es el que usa la funcion WiFi.begin()
- ******************************************************************************************************************
- *** USO DEL LED INTEGRADO DEL ESP
- #define LED_BUILTIN 2
- Por defecto esta puesto el pin 16, pero hay que cambiarlo al 2 en el modulo NodeMCU v3
- ******************************************************************************************************************
- *** 
+ //WiFi.begin(ssid.c_str(), pass.c_str());
+ Redex = Red WiFi externa Control<->PC
+ Redin = Red WiFi interna Control<->Comunicacion
 */
-
 #include <ESP8266WiFi.h>
+#include <Ticker.h>
+#include <EEPROM.h>
+#include "FS.h"
 
-#define LED_BUILTIN 2
-
-//String ssid = "Novcoker";
-//String pass = "54Xocay7JcMa";
-
-IPAddress ipcon(192,168,1,183);
-IPAddress subnetcon(255,255,255,0);
-IPAddress gatecon(192,168,1,1);
-
+// *** VARIABLES GLOBALES ***
 WiFiServer servidor(80);
-WiFiClient cliente;
 
-String paginaWeb(String html){
-  String web;
-  String header = "";
-  header += "HTTP/1.1 200 OK\n";
-  header += "Content-Type: text/html\n";
-  header += "Connection: close\n";
-  header += "Refresh: 5\n";
-  header += "\n";
-  header += "<!DOCTYPE HTML>\n";
-  header += "<head>\n";
-  header += "<title>ConexionPC-MCU</title>";
-  header += "</head>\n";
-  header += "<html>\n";
-
-  web = header;
-  web += html;
-  web += "</html>";
-  return web;
-}
-
-// Conexion a red
-void conexionRed(){
-  WiFi.begin(ssid.c_str(), pass.c_str());
-  int tiempo = millis();
-  while((millis() - tiempo) < 10000){
-    if (WiFi.isConnected()){
-      break;
+// Funcion para obtener el archivo html de una pagina web almacenado en SPIFF
+String getHTML(String archivo){
+  SPIFFS.begin();
+  if(SPIFFS.exists(archivo)){
+    String web = "";
+    File f = SPIFFS.open(archivo, "r");
+    while(f.available()){
+      web += (char) f.read();
     }
-    delay(500);
+    f.close();
+    SPIFFS.end();
+    return web;
   }
-  if (WiFi.isConnected()){
-    while(WiFi.isConnected()){
-      conectado(); 
+  SPIFFS.end();
+  return "";
+}
+
+// Funcion Respuesta del servidor, como pagina web
+String paginaWeb(String html){ 
+  String request = "";
+  String head = "";
+  head += "HTTP/1.1 200 OK\r\n";
+  head += "Content-Type: text/html\r\n";
+  head += "Connection: close\r\n";
+  head += "\r\n";
+  request += head;
+  request += html;
+  return request;
+}
+
+//***************************************************************************************
+//CLASE PUNTO DE ACCESO CONTROL --> RED INTERNA
+class ControlAP{
+  private:
+    // Datos red interna
+    String ssid; // ssid de AP
+    String pass; // pass de AP
+    IPAddress ip;
+    IPAddress gate;
+    IPAddress subnet;
+    
+    // Datos red externa
+    String datosRedex; // Datos obtenidos por web de la red externa
+    
+    // Otros
+    int eeprom; // Bytes reservados para memoria eeprom
+    bool ap_estado; // Estado del punto de acceso True = on, False = off
+
+    //2) Funciones Almacen de Redex
+    bool guardarRedex(String); // Guardar en EEPROM los datos de la red externa
+    void borrarRedex(int); // Borrar EEPROM, para cambiar los datos de la red externa
+    
+    //3) Funciones Obtencion de datos de Redex
+    String clienteDatosRedex(WiFiClient); // Funcion que obtiene los datos de la Redex de un cliente web(PC)
+    void getDatosRedex(String); // Funcion que separa los datos de la Redex pasados por el cliente del resto de datos
+    
+  public:
+    ControlAP();
+
+    //1) Funciones Control AP
+    void iniciarAP(); // Funcion que inicia el AP
+    bool getStaConectados(); // Funcion que comprueba si hay o no sta conectados True = sta On, False = sta OFF
+    bool getEstado(); // Funcion que muestra el estado del AP
+
+    //2) Funciones Almacen de Redex
+    int mem_eeprom(); // Funcion que devuelve la cantidad de memoria eeprom activada (en este caso 128 bytes)
+    String leerRedex(); // Leer EEPROM para obtener los datos de la red externa
+
+    //3) Funciones Obtencion de datos de Redex
+    void Redex(); // Funcion para guardar los datos de la red externa, agrupados en una sola String en forma: ssid=XXX&pass=XXX&
+};
+
+ControlAP::ControlAP(){
+  IPAddress ip(192,168,5,1);
+  IPAddress gate(192,168,5,1);
+  IPAddress subnet(255,255,255,0);
+  this->ip = ip;
+  this->gate = gate;
+  this->subnet = subnet;
+  
+  this->ssid = "Control";
+  this->pass = "123456789";
+  this->datosRedex = "";
+  this->eeprom = 128;
+
+  EEPROM.begin(this->eeprom);
+}
+
+//1) Funciones Control AP
+void ControlAP::iniciarAP(){
+  if(!WiFi.softAP(this->ssid.c_str(), this->pass.c_str())){
+    this->ap_estado = false;
+  }
+  if(!WiFi.softAPConfig(this->ip, this->gate, this->subnet)){
+    this->ap_estado = false;
+  }
+  this->ap_estado = true;
+}
+
+bool ControlAP::getStaConectados(){
+  if(!this->ap_estado){
+    return false;
+  }
+  if(WiFi.softAPgetStationNum() == 0){
+    return false;
+  }
+  return true;
+}
+
+bool ControlAP::getEstado(){
+  return this->ap_estado;
+}
+
+//2) Funciones Almacen de Redex
+int ControlAP::mem_eeprom(){
+  return this->eeprom;
+}
+
+bool ControlAP::guardarRedex(String datos){
+  this->borrarRedex(this->eeprom);
+  if ((datos.indexOf("ssid=") > -1) && (datos.indexOf("pass=") > -1)){
+    for (int i=0; i<datos.length(); i++){
+      EEPROM.write(i, datos[i]);
     }
-  } else {
-    noConectado();
+    EEPROM.commit();
+    return true;
+  }
+  return false;
+}
+
+void ControlAP::borrarRedex(int n_bytes){
+  for (int i=0; i<n_bytes; i++){
+    EEPROM.write(i,0);
+  }
+  EEPROM.commit();
+}
+
+String ControlAP::leerRedex(){
+  int i=0;
+  String s = "";
+  while (((char) EEPROM.read(i)) != '0'){
+    s += (char) EEPROM.read(i);
+    i++;
+  }
+  return s;
+}
+
+//3) Funciones Obtencion de datos de Redex
+String ControlAP::clienteDatosRedex(WiFiClient cliente){
+  String s = "";
+  while(cliente.available()){ // Captura el mensaje enviado por el cliente(PC) al servidor(Control)
+    s += (char) cliente.read();
+  }
+  return s.substring(s.indexOf("GET"), s.indexOf("\n")); // Separa del mensaje la parte que contiene los datos pasados por GET
+}
+
+void ControlAP::getDatosRedex(String s){
+  if ((s.indexOf("ssid=") > -1)&&(s.indexOf("pass=") > -1)){
+   this->datosRedex = "";
+   this->datosRedex += s.substring(s.indexOf("ssid="), s.indexOf("&", s.indexOf("ssid="))+1);
+   this->datosRedex += s.substring(s.indexOf("pass="), s.indexOf("&", s.indexOf("pass="))+1);
   }
 }
 
-// MODO CONECTADO A LA RED WiFi
-void conectado(){
-  Serial.println("Estado: CONECTADO.");
-  aviso_led(WiFi.status());
-  Serial.println("Servidor activo.");
-  Serial.print("Direccion IP: ");
-  Serial.println(WiFi.localIP());
-
-  cliente = servidor.available();
-  if (cliente){
-    String s = "";
-    Serial.println("Cliente conectado.");
-    while(cliente.connected()){
-      if(cliente.available()){
-        s += (char) cliente.read();
-      } else {
-        if (s.indexOf("?") > -1){
-          int i_ini = s.indexOf("[")+1;
-          int i_fin = s.indexOf("]");
-          s = s.substring(i_ini, i_fin);
-          Serial.print("Dato del cliente: ");
-          Serial.println(s);
-          if (s.equals("on") || s.equals("ON")){
-            Serial.println("LED ON.");
-            digitalWrite(LED_BUILTIN, LOW);
-            delay(5000);
-            digitalWrite(LED_BUILTIN, HIGH);
-          }
-        }
-        String pag = "<h1>PAGINA DE PRUEBA... --> H1</h1>";
-        cliente.println(paginaWeb(pag));
-        if (s.equals("wifioff")){
-          WiFi.disconnect();
-        }
-        break;
-      }
-    }
-    delay(100);
-    cliente.stop();
-    Serial.println("Cliente desconectado.");
-  } else {
-    Serial.println("No hay clientes");
-    delay(3000);
+void ControlAP::Redex(){
+  WiFiClient cliente = servidor.available();
+  if (cliente){ // Comprueba si hay cliente web (PC)
+    String datosCliente = this->clienteDatosRedex(cliente); // Obtiene peticion de cliente (PC)
+    this->getDatosRedex(datosCliente); // Separa los datos pasados por PC (SSID y PASS de la red)
+    cliente.println(paginaWeb(getHTML("/web.html"))); // El servidor responde a cliente (control) con la pagina web
+    cliente.stop(); // El cliente para la comunicacion
+  }
+  if(this->datosRedex != ""){
+    this->guardarRedex(this->datosRedex);
   }
 }
 
-// MODO NO CONECTADO A LA RED WiFi
-void noConectado(){
-  Serial.println("Estado: NO CONECTADO.");
-  aviso_led(WiFi.status());
-  delay(250);
-  WiFi.disconnect();
-  delay(250);
+//***************************************************************************************
+//CLASE ESTACION CONTROL --> RED EXTERNA
+class ControlEstacion{
+  private:
+    String ssid; // ssid externo
+    String pass; // pass externo
+
+    // 2) Funciones de Control de ssid y pass
+    String verSSID(); // Funcion que devuelve el ssid registrado de la red externa
+    String verPASS(); // Funcion que devuelve el pass registrado de la red externa
+    String getSSID(String); // Funcion que obtiene el dato del SSID de la red a conectar el modulo de control, dentro de los datos de la red almacenados
+    String getPASS(String); // Funcion que obtiene el dato del PASS de la red a conectar el modulo de control, dentro de los datos de la red almacenados
+  public:
+    ControlEstacion();
+    
+    // 1) Funciones de Redex
+    // 2) Funciones de Control de ssid y pass
+    void obtenerRedex(String); // Obtiene los datos de la Redex, almacenandolos en las variables ssid y pass
+};
+
+ControlEstacion::ControlEstacion(){
+  this->ssid = "";
+  this->pass = "";
 }
 
-// Aviso del estado de la conexion a la red WiFi mediante parpadeos de LED segun el estado
-void aviso_led(int est_conexion){
-  for (int i=0; i<est_conexion; i++){
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(1000);
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
-  }
-  digitalWrite(LED_BUILTIN, HIGH);
+// 2) Funciones de Control de ssid y pass
+String ControlEstacion::verSSID(){
+  return this->ssid;
 }
 
+String ControlEstacion::verPASS(){
+  return this->pass;
+}
+
+String ControlEstacion::getSSID(String s){
+  String ssid = "";
+  int i_ini = s.indexOf("=", s.indexOf("ssid=")) + 1; // Principio del dato
+  int i_fin = s.indexOf("&", s.indexOf("ssid=")); // Final del dato
+  ssid = s.substring(i_ini, i_fin); // SSID
+  return ssid;
+}
+
+String ControlEstacion::getPASS(String s){
+  String pass = "";
+  int i_ini = s.indexOf("=", s.indexOf("pass=")) + 1; // Principio del dato
+  int i_fin = s.indexOf("&", s.indexOf("pass=")); // Final del dato
+  pass = s.substring(i_ini, i_fin); // PASS
+  return pass;
+}
+
+void ControlEstacion::obtenerRedex(String s){
+  this->ssid = this->getSSID(s);
+  this->pass = this->getPASS(s);
+}
+
+//***************************************************************************************
+// *** PROGRAMA ****
 void setup() {
   Serial.begin(115200);
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
-  servidor.begin();
 }
 
 void loop() {
-  conexionRed();
 }
